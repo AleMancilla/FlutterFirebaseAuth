dea490a654f1cacf680649b4fef1ab7d87a28d1a
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-05-30 23:00:55 -0400

    autenticacion por google y guardar en firebase

--- lib/services/AuthService.dart
+++ lib/services/AuthService.dart
@@ -2,17 +2,40 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService{
-  GoogleSignIn _googleSignIn = GoogleSignIn(
-    scopes: <String>[
-      'email',
-      'http://www.google.com/auth/contacts.readonly'
-    ]
-  );
+
+  bool isSignIn = false;
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseUser _user ;
+  //GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  // Future<void> handleSingIn ()async { // encargarse de iniciar session
+  // //https://www.youtube.com/watch?v=deJXYjEH6cs&t=753s
+  // GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
+  // GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+  // AuthCredential credential = GoogleAuthProvider.getCredential(
+  //   idToken: googleSignInAuthentication.idToken,
+  //   accessToken: googleSignInAuthentication.accessToken
+  // );
+  // AuthResult result = (await _auth.signInWithCredential(credential));
+  // _user = result.user;
+  // 
+  // }
+
+  GoogleSignIn _googleSignIn = GoogleSignIn();
 
 Stream<GoogleSignInAccount> get onGoogleCurrentUserChanged  => _googleSignIn.onCurrentUserChanged;
 
 Future<void> singInWithGoogle() async {
-  await _googleSignIn.signIn();
+  // await _googleSignIn.signIn();
+  GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
+  GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+  AuthCredential credential = GoogleAuthProvider.getCredential(
+    idToken: googleSignInAuthentication.idToken,
+    accessToken: googleSignInAuthentication.accessToken
+  );
+  AuthResult result = (await _auth.signInWithCredential(credential));
+  _user = result.user;
+  
 }
 
 Future<void> singInFirebase(String email, String pass)async{
